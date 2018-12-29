528d9c98c2cc96832904c171f9c922b2e76f778d
Inspirationblog <45233679+Inspirationblog@users.noreply.github.com>
2018-12-30 00:04:32 +0200

    website_2

---
+++ main_handler.php
@@ -0,0 +1,13 @@
+<?php 
+if(isset($_POST['submit'])){
+    $to = "themusic4lt@gmail.com";
+    $from = $_POST['email']; 
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $subject = "Form submission";
+    $subject2 = "Copy of your form submission";
+    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
+    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+
+   }
+?>
